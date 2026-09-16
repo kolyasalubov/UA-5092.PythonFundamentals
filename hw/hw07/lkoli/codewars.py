23bcf87352c0906943219d109b8c1cf851cb367d
Liubov <lkoliasa@gmail.com>
2026-09-16 17:59:01 +0300

    added hw07

---
+++ hw/hw07/lkoli/codewars.py
@@ -0,0 +1,12 @@
+
+
+#task Jenny has written a function that returns a greeting for a user. 
+# However, she's in love with Johnny, and would like to greet him slightly different. 
+# She added a special case to her function, but she made a mistake.
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {0}!".format(name)
+
+#task2
