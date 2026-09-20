0df8fc4387f93ae7c42cbe8e7b40d015ef0b2217
ochura-s <o.chura@senuto.com>
2026-09-20 13:59:40 +0200

    HW07: Add 3 practical tasks 07.1 for ochura-s

---
+++ hw/hw07/ochura-s/task1.py
@@ -0,0 +1,7 @@
+def largest_given_number(a, b):
+    """
+    This function returns the largest number between two given numbers.
+    """
+    return max(a, b)
+
+print(largest_given_number(10, 2))
